479208c76557a7b0878974343cd9559165344360
FelipeAnversa <jdanversa@gmail.com>
2025-12-02 15:20:00 -0300

    💎 createTheme e ThemeProvider aplicados no código

--- my-react-app/src/Buscar.jsx
+++ my-react-app/src/Buscar.jsx
@@ -7,6 +7,8 @@ import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import { Typography } from '@mui/material';
+import { createTheme , ThemeProvider } from '@mui/material';
 
 export default function Buscar() {
     const [jogador, setJogador] = useState('');
@@ -53,54 +55,67 @@ export default function Buscar() {
         backgroundColor: 'background.paper',
     };
 
+    const theme = createTheme({
+        pallete: {
+            background: {
+                paper: '#fff',
+            },
+            status: {
+                danger: '#f00',
+            },
+        }
+    });
+
     return (
-        <div style={{fontFamily: ' "Arial", "Helvetica", "sans-serif" ', textAlign: 'center'}}>
-            <div>
-                <TextField 
-                    id='outlined-read-only-input'
-                    placeholder="Escreva Aqui" 
-                    value={jogador} 
-                    onChange={pegarTexto} 
-                />
-            </div>
-            
-            {carregando && <p>Carregando...</p>}
-            
-            {erro && <p style={{color: 'red'}}>{erro}</p>}
-            
-            {selecionado && (
-                <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                    <List sx={style} aria-label='mailbox folders'>
-                        <ListItem>
-                            <ListItemText primary={selecionado.strPlayer} />
-                        </ListItem>
-                        <Divider component='li' />
-                        <ListItem>
-                            <ListItemText primary={selecionado.strTeam} />
-                        </ListItem>
-                        <Divider component='li' />
-                        <ListItem>
-                            <ListItemText primary={selecionado.strNationality} />
-                        </ListItem>
-                        <Divider component='li' />
-                        <ListItem>
-                            <ListItemText primary={selecionado.strPosition} />
-                        </ListItem>
-                        <Divider component='li' />
-                        <ImageList sx={{ width: 725, height: 450 }}>
-                            <ImageListItem>
-                                <img 
-                                    srcSet={`${selecionado.strThumb}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                    src={`${selecionado.strThumb}?w=164&h=164&fit=crop&auto=format`} 
-                                    alt={selecionado.strPlayer}
-                                    loading="lazy"
-                                />
-                            </ImageListItem>
-                        </ImageList>
-                    </List>
+        <ThemeProvider theme={theme}>
+            <div style={{fontFamily: ' "Arial", "Helvetica", "sans-serif" ', textAlign: 'center'}}>
+                <div>
+                    <TextField 
+                        id='outlined-read-only-input'
+                        placeholder="Escreva Aqui" 
+                        value={jogador} 
+                        onChange={pegarTexto} 
+                    />
                 </div>
-            )}
-        </div>
+                
+                {carregando && <p>Carregando...</p>}
+                
+                {erro && <Typography sx={{color: 'status.danger'}}>{erro}</Typography>}
+                
+                {selecionado && (
+                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                        <List sx={style} aria-label='mailbox folders'>
+                            <ListItem>
+                                <ListItemText primary={selecionado.strPlayer} />
+                            </ListItem>
+                            <Divider component='li' />
+                            <ListItem>
+                                <ListItemText primary={selecionado.strTeam} />
+                            </ListItem>
+                            <Divider component='li' />
+                            <ListItem>
+                                <ListItemText primary={selecionado.strNationality} />
+                            </ListItem>
+                            <Divider component='li' />
+                            <ListItem>
+                                <ListItemText primary={selecionado.strPosition} />
+                            </ListItem>
+                            <Divider component='li' />
+                            <ImageList sx={{ width: 725, height: 450 }}>
+                                <ImageListItem>
+                                    <img 
+                                        srcSet={`${selecionado.strThumb}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                        src={`${selecionado.strThumb}?w=164&h=164&fit=crop&auto=format`} 
+                                        alt={selecionado.strPlayer}
+                                        loading="lazy"
+                                    />
+                                </ImageListItem>
+                            </ImageList>
+                        </List>
+                    </div>
+                )}
+            </div>
+        </ThemeProvider>
     );
 }
 
